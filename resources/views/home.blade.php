e7e21328f87e7e11c5442ef94d82db3b25c0716c
Joao Augusto <jpaiaobonifacio@gmail.com>
2020-06-25 00:10:45 -0300

    Página principal

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('content')
+        
+    <div class="row">
+        <h1>História em Quadrinhos</h1>
+        <a href="#" target="_parent">
+            <button class="btn btn-outline-dark ml-3">Criar HQ</button>
+        </a>
+    </div>
+    <hr class="bg-dark"/>
+
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Teste com HQs</title>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+   
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="jumbotron bg-white py-4">
+        @yield('content')
+    </div>
+</body>
+</html>
