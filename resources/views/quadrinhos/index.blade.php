d93afe711352a47de5dd481e8f645fc716022acc
Bruno Luciano <bruno.lucian0544@gmail.com>
2020-07-14 10:49:01 -0300

    Adicionando balao e texto

--- resources/views/quadrinhos/index.blade.php
+++ resources/views/quadrinhos/index.blade.php
@@ -4,6 +4,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>QUADRINHOS</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" integrity="sha512-xA6Hp6oezhjd6LiLZynuukm80f8BoZ3OpcEYaqKoCV3HKQDrYjDE1Gu8ocxgxoXmwmSzM4iqPvCsOkQNiu41GA" crossorigin="anonymous" />
   <link rel="stylesheet" href="{{ asset('css/index.css') }}">
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
@@ -24,16 +25,22 @@
             <div class="col-12">
                 <div class="col-9 acopla-imagens" id="acopla-imagens" style="display: flex; align-items: stretch; z-index:2">
                     
-                    <div class="arrastavel" style="background-image: url('{{ env('APP_URL') }}/storage/{{ $hq->personagem1->personagem }}')"></div>
-                    <div class="arrastavel" style="background-image: url('{{ env('APP_URL') }}/storage/{{ $hq->personagem2->personagem }}')">
+                    <div class="arrastavel personagem" style="background-image: url('{{ env('APP_URL') }}/storage/{{ $hq->personagem1->personagem }}')" oncontextmenu="show_hide();return false;"></div>
+                    <div class="arrastavel personagem" style="background-image: url('{{ env('APP_URL') }}/storage/{{ $hq->personagem2->personagem }}')" oncontextmenu="show_hide();return false;">
                     
                     </div>
                 </div>
 
-                <div style="display: flex;">
+                <div id="baloes" style="display: none;">
                     {{-- https://www.youtube.com/watch?v=w88U9JA70wQ --}}
                     @foreach ($balaos as $balao)
-                        <div class="arrastavel" style="background-image: url('{{ env('APP_URL') }}/storage/{{ $balao->caminho }}')"></div>
+
+
+                        {{-- <textarea rows="3" cols="3" class="arrastavel text-center balaoContent" style="background-image: url('{{ env('APP_URL') }}/storage/{{ $balao->caminho }}'); border:none; outline:none; padding:5px"></textarea> --}}
+
+                        <div class="arrastavel balao p-1" style="background-image: url('{{ env('APP_URL') }}/storage/{{ $balao->caminho }}')">
+                            <textarea rows="3" cols="13" class="text-center balaoContent" style="height:50px; border:none; outline:none; padding:5px; margin: 25px 0px 0px 13px; resize: none; width:80%; height:40%"></textarea>
+                        </div>
                     @endforeach
                 </div>
                 
@@ -78,6 +85,36 @@
         </div>
     </div>
 </div>   
+
+
+<script>
+    function show_hide() {
+        var img = document.getElementById('personagem');
+        // console.log(img);
+        // var button = document.createElement('button');
+        // button.type = "button";
+        // button.classList.add("btn");
+        // button.classList.add("btn-primary");
+        // button.classList.add("marginDoTopo");
+
+        // var i = document.createElement('i');
+        // i.classList.add("fas");
+        // i.classList.add("fa-plus");
+
+        // button.appendChild(i);
+
+        // img.appendChild(button);
+        // console.log(img);
+
+        var baloes = document.getElementById("baloes");
+
+        if(baloes.style.display === "none") {
+            baloes.style.display = "block";
+        } else {
+            baloes.style.display = "none";
+        }
+    }
+</script>
 
 <script src="{{ asset('js/index.js') }}"></script>
 </body>
